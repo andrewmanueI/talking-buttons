d6c355d8ef11a47b9dccd28a24e74b93e82e22cc
andrew <andrewmanuelcool@gmail.com>
2026-05-30 02:35:34 +0700

    Bump version to 1.1.3

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function Background({ settings }: { settings: ReturnType<typeof useSettings>['se
   );
 }
 
-const APP_VERSION = '1.1.2';
+const APP_VERSION = '1.1.3';
 
 function logAppInfo() {
   const isNative = !!(window as any).Capacitor;
